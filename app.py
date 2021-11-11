27b0e0e08d42db8f205a983155c0b773d67f81b0
ifiraso <44394489+ifiraso@users.noreply.github.com>
2021-11-11 22:26:51 +0200

    function adj

--- app.py
+++ app.py
@@ -1,33 +1,40 @@
+from http import HTTPStatus
 import requests as requests
 from flask import Flask, request
 import json
 app = Flask(__name__)
-from http import HTTPStatus
+
 catalog_server_ip = "http://192.168.1.129:3000"
+
+
 @app.route('/purchase', methods=['GET'])
 def purchase():
+    item_number = request.args.get("item_number")
+
     success = {
         "purchase_status": "Sucessful",
-        "status" : HTTPStatus.OK
+        "status": HTTPStatus.OK
     }
-    fails ={
+    fails = {
         "purchase_status": "No Stock",
         "status": HTTPStatus.OK
     }
     success = json.dumps(success)
     fails = json.dumps(fails)
-    item_number = request.args.get("item_number")
-
-    r = requests.get(url=catalog_server_ip+"/check", params=item_number)
-    result = r.json()
-    if result['stock'] ==True:
-        r = requests.get(url=catalog_server_ip + "/purchase/confirm", params=item_number)
+    stock_check = requests.get(
+        url=catalog_server_ip+"/check", params=item_number)
+    stock_check_json = stock_check.json()
+    if stock_check_json['stock'] == True:
+        purchase_confirm = requests.get(url=catalog_server_ip +
+                                        "/update/stock/dec", params=item_number)
         return success
     else:
         return fails
+
+
 @app.route('/')
 def hello_world():
-    return 'Hello World! 5000'
+    return 'Hello World! working ORDER SERVER'
 
 
 if __name__ == '__main__':
