0beddedaa3d5e7a9c6ce5e772bef9a7f8f3160fd
ifiraso <44394489+ifiraso@users.noreply.github.com>
2021-11-12 23:55:31 +0200

    changes

--- app.py
+++ app.py
@@ -2,52 +2,52 @@ import sqlite3
 from flask import Flask, request
 import json
 import requests as requests
-from flask.wrappers import Response
-from werkzeug.wrappers import response
 app = Flask(__name__)
 
-catalog_server = "http://192.168.1.129:3000"
-order_server = "http://192.168.1.129:5000"
+catalog_server = "http://192.168.56.103:5000"
+order_server = "http://192.168.56.104:5000"
 
 
 @app.route('/search/topic', methods=['GET'])
 def query_by_subject_search():
     topic_name = request.args.get("name")
-    response = requests.get(url=catalog_server+"/topic", params=topic_name)
-    return response
+    response = requests.get(
+        url=catalog_server+"/search/topic", params={"name": topic_name})
+    return response.json()
 
 
 @app.route('/search/itemnumber', methods=['GET'])
 def search_by_item_number():
     item_number = request.args.get("item_number")
     response = requests.get(
-        url=catalog_server+"/itemnumber", params=item_number)
-    return response
+        url=catalog_server+"/search/itemnumber", params={"item_number": item_number})
+    return response.json()
 
 
-@app.route('/update/stock', methods=['GET'])
+@app.route('/update/stock', methods=['PATCH'])
 def update_stock():
-    id_item = request.args.get("book_id")
+    id_item = request.args.get("item_number")
     stock = request.args.get("stock")
     response = requests.get(
-        url=catalog_server+"/update/stock", params={id_item, stock})
-    return response
+        url=catalog_server+"/update/stock", params={"item_number": id_item, "stock": stock})
+    return response.json()
 
 
-@app.route('/update/cost', methods=['GET'])
+@app.route('/update/cost', methods=['PATCH'])
 def update_cost():
-    id_item = request.args.get("book_id")
+    id_item = request.args.get("item_number")
     cost = request.args.get("cost")
     response = requests.get(
-        url=catalog_server+"/update/stock", params={id_item, cost})
-    return response
+        url=catalog_server+"/update/cost", params={"item_number": id_item, "cost": cost})
+    return response.json()
 
 
-@app.route('/purchase', methods=['GET'])
+@app.route('/purchase', methods=['POST'])
 def purchase():
     item_number = request.args.get("item_number")
-    response = requests.get(url=order_server+"/purchase", params=item_number)
-    return response
+    response = requests.get(url=order_server+"/purchase",
+                            params={"item_number": item_number})
+    return response.json()
 
 
 @app.route('/')
